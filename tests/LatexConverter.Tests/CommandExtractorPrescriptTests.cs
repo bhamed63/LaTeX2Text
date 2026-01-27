cd5552afd35999b8a8a730862b90eedf9150b47f
Hamed <b.hamed.63@gmail.com>
2026-01-27 15:23:02 +0330

    aa

--- tests/LatexConverter.Tests/CommandExtractorPrescriptTests.cs
+++ tests/LatexConverter.Tests/CommandExtractorPrescriptTests.cs
@@ -33,5 +33,19 @@ namespace LatexConverter.Tests
 
             Assert.Contains("U", mathrmArgs);
         }
+
+        [Fact]
+        public void ExtractCommands_ShouldHandlePrescriptNode_When_It_Contains_Number()
+        {
+            var parser = new LatexParser();
+            var nodes = parser.Parse("^{235}U");
+            var extractor = new LatexCommandExtractor();
+            var commands = extractor.ExtractCommands(nodes);
+
+            // Should extract "prescript" and potentially "U" as an argument
+            Assert.Contains(commands, c => c.CommandName == "prescript");
+            var prescriptCmd = commands.First(c => c.CommandName == "prescript");
+            Assert.Contains("U", prescriptCmd.TextArguments);
+        }
     }
 }
